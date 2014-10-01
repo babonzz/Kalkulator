6552e3cfde8e8d22f6d09f2834089e8b195e484f
infonusateknologiprima <infonusateknologiprima@gmail.com>
2014-10-01 13:00:56 +0700

    Menambah function sum dan subtract

---
+++ Kalkulator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ITP
+ */
+public class Kalkulator {
+    
+    public int sum(int a, int b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    
+}
+
