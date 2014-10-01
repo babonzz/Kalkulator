728eba38f1ffa73b8c4dd7412de8e7986bd42354
infonusateknologiprima <infonusateknologiprima@gmail.com>
2014-10-01 13:28:59 +0700

    add multiplyArray function

--- Kalkulator.java
+++ Kalkulator.java
@@ -44,5 +44,13 @@ public class Kalkulator {
         return result;
     }   
     
+    public int multiplyArray(int[] arr) {
+        int result = 0;
+        for(int i = 0; i < arr.length; i++) {
+            result *= arr[i];
+        }
+        return result;
+    }
+    
 }
 
