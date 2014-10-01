eecfc797f543027a206d9b7addf4ad8af01aeb12
Denny W <denny.winoto@berkeley.edu>
2014-10-01 13:08:34 +0700

    bikin fungsi multiply dan divide

--- Kalkulator.java
+++ Kalkulator.java
@@ -17,6 +17,14 @@ public class Kalkulator {
     public int subtract(int a, int b) {
         return a - b;
     }
+
+    public int multiply(int a, int b) {
+    	return a * b;
+    }
+
+    public int divide(int a, int b) {
+    	return a / b;
+    }
     
 }
 
